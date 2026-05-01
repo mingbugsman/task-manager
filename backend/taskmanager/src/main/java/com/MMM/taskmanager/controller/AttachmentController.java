65a565a1c56ec783f0d864b05b04b983866bdee7
Tran Tuan Minh <ming18380@gmail.com>
2026-05-01 22:03:12 +0700

    feat(attachment): write controller attachment

---
+++ backend/taskmanager/src/main/java/com/MMM/taskmanager/controller/AttachmentController.java
@@ -0,0 +1,67 @@
+package com.MMM.taskmanager.controller;
+
+import com.MMM.taskmanager.dto.response.attachment.AttachmentResponse;
+import com.MMM.taskmanager.dto.response.util.ApiResponse;
+import com.MMM.taskmanager.service.AttachmentService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequestMapping("/api/v1")
+public class AttachmentController {
+    AttachmentService attachmentService;
+
+    @GetMapping("/{entityType}/{entityId}/attachments")
+    public ResponseEntity<ApiResponse<List<AttachmentResponse>>> getAttachments(
+            @PathVariable String entityType,
+            @PathVariable Long entityId) {
+
+        List<AttachmentResponse> result = attachmentService.getAttachments(entityType, entityId);
+        return ResponseEntity.ok(ApiResponse.ok(result));
+    }
+
+
+    // POST /api/v1/{entityType}/{entityId}/attachments
+    @PostMapping(value = "/{entityType}/{entityId}/attachments",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<AttachmentResponse>> uploadAttachment(
+            @PathVariable String entityType,
+            @PathVariable Long entityId,
+            @RequestPart("file") MultipartFile file) {
+
+        AttachmentResponse result = attachmentService.uploadAttachment(entityType, entityId, file);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.created(result));
+    }
+
+    // DELETE /api/v1/attachments/{attachmentId}
+    @DeleteMapping("/attachments/{attachmentId}")
+    public ResponseEntity<ApiResponse<Void>> deleteAttachment(
+            @PathVariable Long attachmentId) {
+
+        attachmentService.deleteAttachment(attachmentId);
+        return ResponseEntity.ok(ApiResponse.ok("Attachment deleted successfully"));
+    }
+
+    // GET /api/v1/attachments/{attachmentId}/download
+    @GetMapping("/attachments/{attachmentId}/download")
+    public ResponseEntity<Void> downloadAttachment(
+            @PathVariable Long attachmentId) {
+
+        String fileUrl = attachmentService.getDownloadUrl(attachmentId);
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(fileUrl))
+                .build();
+    }
+}
