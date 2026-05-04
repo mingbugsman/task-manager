869d177ed878a8e43d4f07bf7eb40b7a7d5107c9
Tran Tuan Minh <ming18380@gmail.com>
2026-05-04 17:53:09 +0700

    feat(project): add project service crud and security u;ti check admin

---
+++ backend/taskmanager/src/main/java/com/MMM/taskmanager/service/ProjectService.java
@@ -0,0 +1,25 @@
+package com.MMM.taskmanager.service;
+
+import com.MMM.taskmanager.dto.request.project.ProjectRequest;
+import com.MMM.taskmanager.dto.request.project.UpdateProjectStatusRequest;
+import com.MMM.taskmanager.dto.response.project.BroadResponse;
+import com.MMM.taskmanager.dto.response.project.ProjectDetailResponse;
+import com.MMM.taskmanager.dto.response.project.ProjectOverallStatsResponse;
+import com.MMM.taskmanager.dto.response.project.ProjectSummaryResponse;
+import com.MMM.taskmanager.dto.response.util.PageResponse;
+
+public interface ProjectService {
+    PageResponse<ProjectSummaryResponse> getProjects(String search, int page, int size);
+    ProjectDetailResponse getProjectDetail(Long projectId);
+    ProjectOverallStatsResponse getProjectStats();
+    BroadResponse getBoardByProjectId(Long projectId, Long assigneeId, Long labelId);
+
+    // crud project
+    ProjectDetailResponse createProject(ProjectRequest request);
+    ProjectDetailResponse updateProject(Long projectId, ProjectRequest request);
+    ProjectDetailResponse updateProjectStatus(Long projectId, UpdateProjectStatusRequest request);
+    ProjectDetailResponse restoreProject(Long projectId);
+    void deleteProject(Long projectId);
+
+
+}
--- backend/taskmanager/src/main/java/com/MMM/taskmanager/util/SecurityUtils.java
+++ backend/taskmanager/src/main/java/com/MMM/taskmanager/util/SecurityUtils.java
@@ -1,10 +1,20 @@
 package com.MMM.taskmanager.util;
 
+import com.MMM.taskmanager.entity.User;
 import com.MMM.taskmanager.entity.UserDetailsImpl;
+import com.MMM.taskmanager.exception.AppException;
+import com.MMM.taskmanager.exception.ErrorCode;
+import com.MMM.taskmanager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Objects;
+
 public class SecurityUtils {
+
+    static UserRepository userRepository;
+
     public static Long getCurrentUserId() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth == null || !auth.isAuthenticated() || auth.getPrincipal().equals("anonymousUser")) {
@@ -12,5 +22,11 @@ public class SecurityUtils {
         }
         UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
         return userDetails.getId();
+    }
+
+    public static boolean isAdmin() {
+        Long userId = getCurrentUserId();
+        User user = userRepository.findById(userId).orElseThrow(() -> new AppException(ErrorCode.FORBIDDEN));
+        return Objects.equals(user.getRoleGlobal(), "ADMIN");
     }
 }
