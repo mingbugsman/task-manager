0e332c3e5c977cefca70370425237ad9a93371fa
Tran Tuan Minh <ming18380@gmail.com>
2026-05-21 12:28:39 +0700

    update security config

--- backend/taskmanager/src/main/java/com/MMM/taskmanager/security/SecurityConfig.java
+++ backend/taskmanager/src/main/java/com/MMM/taskmanager/security/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://task-manager-henna-alpha.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
