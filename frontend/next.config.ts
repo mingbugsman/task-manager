f4ba42335b873f9882d6b3091494b32cc32d1f2c
Tran Tuan Minh <ming18380@gmail.com>
2026-05-21 16:21:31 +0700

    fix error desktop release

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.BUILD_TARGET === "desktop";
+
 const nextConfig: NextConfig = {
+  output: isProd ? "export" : undefined,
+  images: {
+    unoptimized: isProd ? true : undefined,
+  },
 };
 
 export default nextConfig;
